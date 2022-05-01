a82cca47dbb047f58ae5c2f322a0f22e7abaa14d
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-05-01 15:39:08 -0600

    Introduce Tailwinds Starter Code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import Navbar from './Navigation/Navbar.js'
+import Example from './Example/Example'
+function App() {
+  return (
+    <div>
+      <Navbar />
+      < Example />
+    </div>
+  );
+}
+
+export default App;
